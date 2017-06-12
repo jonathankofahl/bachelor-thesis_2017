fcd6f40c41e77f1af88529ccf8ce84b62d5b3fc0
jonathankofahl <s59275@beuth-hochschule.de>
2017-06-12 10:16:46 +0200

    init Xcode Project

---
+++ BA_JonathanKofahl/BA_JonathanKofahl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BA_JonathanKofahl
+//
+//  Created by Jonathan Kofahl on 12.06.17.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
